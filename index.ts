7e883697849355490c259099b56e417a3574cb6a
samina <saminaharoon858@gmail.com>
2024-03-22 00:02:23 +0500

    i added tow words in line number 5 and 6 and push my calculator to github

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNamber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter my first number", type: "number", name: "firstNamber" },
+    { message: "Enter  my second number", type: "number", name: "secondNumber" },
     {
        message: "Select one of the operators to perform operation",
         type: "list",
